339635e6acb222eb07873b46e9a037c20716e4d6
balabanEren <balabaneren1631@gmail.com>
2024-10-30 17:42:40 +0300

    first e-commerce commit

---
+++ E-commerceJs/mainPage.js
@@ -0,0 +1,47 @@
+let allSlides=document.querySelectorAll(".slides .slide")
+let currentSlideİndex=0
+let forwardSlide=document.querySelector(".forwardSlide")
+let backSlide=document.querySelector(".backSlide")
+
+
+
+function SlideTrigger(){
+    allSlides[currentSlideİndex].classList.add("display")
+    setInterval(slideAuto,3000)
+}
+
+function slideAuto(){
+
+    allSlides[currentSlideİndex].classList.remove("display")
+    currentSlideİndex++
+        if(currentSlideİndex>=allSlides.length){
+            currentSlideİndex=0
+        }
+        allSlides[currentSlideİndex].classList.add("display")
+}
+
+
+forwardSlide.addEventListener("click",()=>{
+    slideAuto();
+})
+
+///
+
+backSlide.addEventListener("click",()=>{
+    
+   
+    
+    allSlides[currentSlideİndex].classList.remove("display");
+    currentSlideİndex--;
+ 
+    if (currentSlideİndex < 0) {
+        currentSlideİndex = allSlides.length - 1; 
+        console.log("0 da")
+    }
+ 
+    allSlides[currentSlideİndex].classList.add("display");
+})
+
+
+SlideTrigger()
+setInterval(slideAuto,4000)
